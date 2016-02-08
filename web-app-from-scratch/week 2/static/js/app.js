48d956a61ef9855b316b490f7d243ae581a19676
Lisa Klein <lisa.klein@rtl.nl>
2016-02-08 22:19:01 +0100

    some small html & css changes

--- web-app-from-scratch/week 2/static/js/app.js
+++ web-app-from-scratch/week 2/static/js/app.js
@@ -1,3 +1,56 @@
+// maak een namespace om conflicten te voorkomen
+var webApp = webApp || {}; 
+
+// IIFE (self invoking function)
+(function(){
+    // use strict modus aanzetten -> stuurt geen var door naar globale scope
+    'use strict';
+    
+    // Object Literal app
+    webApp.app = {
+        // method: init
+        init: function(){
+            webApp.routes.init();
+        }
+    };
+    
+    // Object Literal routes
+    webApp.routes = {
+        init: function(){ 
+            // on hashchange roept sections.toggle aan
+            window.addEventListener("hashchange",webApp.sections.toggle);
+            // voert functie uit bij window load
+            window.addEventListener("load", webApp.sections.toggle);
+        }
+    };
+    
+    // Object Literal sections
+    webApp.sections = {
+        toggle: function(route){
+            var sections = document.querySelectorAll(".togglesection"),
+                route = window.location.hash;
+
+            for (var i = 0; i < sections.length; i++) {
+                // add inactive aan alle sections
+                sections[i].classList.add('inactive');
+            
+                // if geen # in de link
+                if (!route) {
+                    sections[0].classList.remove('inactive');
+                
+                } else if(window.location.hash === "#" + sections[i].id) {
+                    sections[i].classList.remove('inactive');
+                }
+            }  
+        }
+    };
+    
+    webApp.app.init();
+
+})();
+
+
+
 (function(){
     'use strict';
     
